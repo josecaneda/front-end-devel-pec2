a1a025cf1d5afd9cd06b3e223a1d9d684854d64a
jose caneda <jcaneda@uoc.edu>
2022-03-18 00:04:50 +0100

    PEC2_EJ3 a_map solved

--- PEC2_Ej3/a_map/core.js
+++ PEC2_Ej3/a_map/core.js
@@ -1,17 +1,46 @@
 function multiplyBy10(array) {
   // your code here
+  return array.map( x => x * 10);
 }
 
 function shiftRight(array) {
   // your code here
+  return  array.map( (_value, _index, _array) => {
+    if (_index == 0) {
+      return _array[_array.length - 1];
+    } else {
+      return _array[_index - 1];
+    }
+
+  });
 }
 
 function onlyVowels(array) {
   // your code here
+  return array.map(value => {
+    return [...value].map(_value => {
+      switch(_value) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+          return _value;
+      }
+    }).join('');
+  });
 }
 
 function doubleMatrix(array) {
   // your code here
+  return array.map(value => {
+    return value.map(_value =>  _value * 2);
+  });
 }
 
 module.exports = {
