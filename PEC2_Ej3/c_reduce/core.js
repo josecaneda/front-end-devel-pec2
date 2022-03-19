fd114ea3d43637b06d2423d2a75eb91cbb946270
jose caneda <jcaneda@uoc.edu>
2022-03-19 14:18:43 +0100

    PEC2_EJ3 a_reduced solved

--- PEC2_Ej3/c_reduce/core.js
+++ PEC2_Ej3/c_reduce/core.js
@@ -1,17 +1,36 @@
 function sum(array) {
   // your code here
+  const initialValue = 0;
+  return array.reduce( (previousValue, currentValue) => previousValue + currentValue, initialValue);
+
 }
 
 function productAll(array) {
   // your code here
+  const initialValue = 1;
+  return array.reduce( (previousValue, currentValue) => previousValue *
+    [...currentValue].reduce( (_previousValue, _currentValue) => _previousValue * _currentValue, initialValue )
+    , initialValue );
+
 }
 
 function objectify(array) {
   // your code here
+  return Object.fromEntries(array);
 }
 
 function luckyNumbers(array) {
   // your code here
+  const initialValue = 'Your lucky numbers are:';
+  let selectSeparator = (index, arrayLength) => {
+    if (index + 1 == arrayLength) return '';
+    if (index + 2 == arrayLength) return ', and';
+    return ','
+  };
+  
+  return array.reduce( (previousValue, currentValue, index) => 
+    `${previousValue} ${currentValue}${selectSeparator(index, array.length)}`
+    , initialValue);
 }
 
 module.exports = {
