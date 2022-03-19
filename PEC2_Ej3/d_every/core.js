f865fd0ec84472b63e57b80beb2a24dc5754a1f0
jose caneda <jcaneda@uoc.edu>
2022-03-19 14:38:43 +0100

    PEC2_EJ3 c_every solved

--- PEC2_Ej3/d_every/core.js
+++ PEC2_Ej3/d_every/core.js
@@ -2,14 +2,15 @@
 // are even numbers.
 
 function allEven(input) {
-  return input;
+  return input.every( value => value % 2 == 0);
 }
 
 // Check to see if all elements in an array
 // are of the same type.
 
 function allSameType(input) {
-  return input;
+  const type = typeof(input[0]); 
+  return input.every( value => typeof(value) === type );
 }
 
 // Check to see if every element in the matrix is
@@ -17,14 +18,27 @@ function allSameType(input) {
 // greater than 0.
 
 function positiveMatrix(input) {
-  return input;
+  return input.every( value => [...value].every( _value => _value >= 0 ));
 }
 
 // Check that all items in an array are strings
 // and that they all only contain the same vowels.
 
 function allSameVowels(input) {
-  return input;
+  return input.every( value => {
+    let searchVowel = 0;
+    let result = false
+    if (value.indexOf('a') > -1 )  searchVowel +=  1;
+    if (value.indexOf('e') > -1 )  searchVowel +=  2;
+    if (value.indexOf('i') > -1 )  searchVowel +=  8;
+    if (value.indexOf('o') > -1 )  searchVowel +=  16;
+    if (value.indexOf('u') > -1 )  searchVowel +=  32;
+    
+    if (searchVowel === 1 || searchVowel === 2 || searchVowel === 8 || searchVowel === 16 || searchVowel === 32) {
+      result = true;
+    } 
+    return result;
+  });
 }
 
 module.exports = {
