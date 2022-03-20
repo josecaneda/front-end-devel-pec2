0ed1f40f7f303adafebc4fbf28fedc1f3c2ebafe
jose caneda <jcaneda@uoc.edu>
2022-03-20 23:37:56 +0100

    PEC2_EJ3 f_zoo solved

--- PEC2_Ej3/f_zoo/core.js
+++ PEC2_Ej3/f_zoo/core.js
@@ -13,12 +13,11 @@ function entryCalculator(entrants) {
     (previousValue, [type, amount]) => previousValue + data.prices[type] * amount,
     initialValue
   );
-  
 }
+
 
 function schedule(dayName) {
   // your code here
-
   const convert24HourToAmPM = (hour) => {
     return hour < 12 ? `${hour}am` : `${hour - 12}pm`;
   };
@@ -33,6 +32,7 @@ function schedule(dayName) {
   return Object.fromEntries(resultArray);
 }
 
+
 function animalCount(species) {
   // your code here
   const animalCountResult = animals.reduce( (totalAnimals, {name, residents}) => {
@@ -42,17 +42,39 @@ function animalCount(species) {
     , {});
 
     return species ? animalCountResult[species] : animalCountResult;
-
 }
+
 
 function animalMap(options) {
   // your code here
   const initialValue = { NE: [], NW: [], SE: [], SW: [] };
-  return animals.reduce( (totalLocations, {name, location}) => {
-    totalLocations[location] = [...totalLocations[location], name];
-    return totalLocations;
-  }, initialValue);
+  return animals.reduce( (totalLocations, {name, location, residents} ) => {
+    let animalsNames;
+    let addElement = true;
+    try {
+      if (options.sex && options.includeNames) {
+        animalsNames = {};
+        animalsNames[name] = residents.filter(( {sex} ) => sex === options.sex ).map( ({name}) => name );
+      } else if (options.includeNames) {
+        animalsNames = {};
+        animalsNames[name] =  residents.map( ({name}) => name );
+      } else if (options.sex) {
+        addElement = residents.some( ({sex}) => sex === options.sex );
+        animalsNames = name;
+        console.log(animalsNames);
+      } else {
+        animalsNames = name;
+      }
+    } catch (error) {
+      animalsNames = name;
+    }
 
+    if (addElement) {
+      totalLocations[location] = [...totalLocations[location], animalsNames];
+    }
+    return totalLocations;
+    
+  }, initialValue);
 }
 
 function animalPopularity(rating) {
@@ -69,12 +91,14 @@ function animalPopularity(rating) {
   return rating ? animalPopularityResult[rating] : animalPopularityResult;
 }
 
+
 function animalsByIds(ids) {
   // your code here
   return animals.filter( ({id}) => {
     if (ids) return ids.includes(id);
   });
 }
+
 
 function animalByName(animalName) {
   // your code here
@@ -88,6 +112,7 @@ function animalByName(animalName) {
   }, {});
 }
 
+
 function employeesByIds(ids) {
   // your code here
   return employees.filter( ({id}) => {
@@ -95,17 +120,18 @@ function employeesByIds(ids) {
   });
 }
 
+
 function employeeByName(employeeName) {
   // your code here
   if (!employeeName) return {};
   return employees.find( ({firstName, lastName}) => firstName === employeeName || lastName === employeeName );
 }
 
+
 function managersForEmployee(idOrName) {
   // your code here
 
   function employeesByIds(ids) {
-    // your code here
     return employees.find( ({id}) => {
       if (ids) return ids.includes(id);
     });
@@ -120,33 +146,33 @@ function managersForEmployee(idOrName) {
   return employee;
 }
 
+
 function employeeCoverage(idOrName) {
   // your code here
-
   function animalsByIds(ids) {
-    // your code here
-    return animals.filter( ({id}) => {
-      if (ids) return ids.includes(id);
-    });
+    return ids.map((_id) => 
+      animals.find(({id}) => id === _id)
+    );
   }
 
   function employeeByNameOrId(employeeNameOrId) {
-    // your code here
     if (!employeeNameOrId) return {};
     const employee = employees.find( ({firstName, lastName, id}) => firstName === employeeNameOrId || lastName === employeeNameOrId || id === employeeNameOrId);
-    return `${employee.firstName} ${employee.lastName}`;
+      return `${employee.firstName} ${employee.lastName}`;
   }
 
-  const employeeCoverageResult =  employees.reduce( (acc, {firstName, lastName, responsibleFor}) => {
+  const employeesCoverageResult =  employees.reduce( (acc, {firstName, lastName, responsibleFor}) => {
     acc[`${firstName} ${lastName}`] = animalsByIds(responsibleFor).map( ({id, name}) => name);
     return acc;
-  }, {});
-    if (idOrName) {
-      let obj = {};
-      obj[employeeByNameOrId(idOrName)] = employeeCoverageResult[employeeByNameOrId(idOrName)];
-      return obj;
-    }
-    return employeeCoverageResult
+    }, {});
+
+  if (idOrName) {
+      let oneEmployeeCoverageResult = {};
+      const employeeName =  employeeByNameOrId(idOrName)
+      oneEmployeeCoverageResult[employeeName] = employeesCoverageResult[employeeName];
+      return oneEmployeeCoverageResult;
+  }
+  return employeesCoverageResult
 }
 
 module.exports = {
